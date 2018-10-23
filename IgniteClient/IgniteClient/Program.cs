27889e7a6feb4e8bc133d2c9de3a50a49b531ccc
Vantalasteiger <andrej.komissarov@gmail.com>
2018-10-22 23:29:24 -0400

    IgnitePrototype

---
+++ IgniteClient/IgniteClient/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.Ignite;
+using Apache.Ignite.Service;
+using Apache.Ignite.Linq;
+using Apache;
+using Apache.Ignite.Core.Cache.Store;
+using Apache.Ignite.Core.Cache;
+using Apache.Ignite.Core;
+using Apache.Ignite.Core.Client;
+
+namespace IgniteClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ignition.ClientMode = true;
+            var cfg = new IgniteClientConfiguration { Host = "127.0.0.1" };
+            using (var client = Ignition.StartClient(cfg))
+            {
+                var cache = client.GetOrCreateCache<int, string>("test");
+                while (true)
+                {
+                    Console.WriteLine("Printing content of the cache");
+                    for (int i = 0; i < 10; i++)
+                    {
+                        if (cache.ContainsKey(i))
+                        {
+                            string obj = cache.Get(i);
+                            Console.WriteLine($"Key={i}, value={obj}");
+                        }
+                    }
+
+                    System.Threading.Thread.Sleep(3000);
+                }
+            }
+
+                
+            //Ignition.StartClient()
+
+            //// Start Ignite in client mode.
+            //IIgnite ignite = Ignition.Start();
+
+            // Create cache on all the existing and future server nodes.
+            // Note that since the local node is a client, it will not 
+            // be caching any data.
+
+        }
+    }
+}
---
+++ IgnitePriceServer/IgnitePriceServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apache.Ignite.Core;
+using Apache.Ignite;
+using Apache.Ignite.Service;
+using Apache.Ignite.Linq;
+using Apache;
+using Apache.Ignite.Core.Cache.Store;
+using Apache.Ignite.Core.Cache;
+
+namespace IgnitePriceServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IIgnite ignite = Ignition.Start();
+            int key = Int32.Parse(args[0]);
+
+            ICache<int, string> cache = ignite.GetOrCreateCache<int, string>("test");
+            cache.Put(key, $"String{key}");
+
+            Console.ReadKey();
+        }
+    }
+}
